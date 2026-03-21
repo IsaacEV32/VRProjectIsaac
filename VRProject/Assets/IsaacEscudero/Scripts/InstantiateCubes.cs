2e645fd4c56bf53d5fdbfd18e4cd81c060933b22
Isaac Escudero <isaac_escudero@euneiz.com>
2026-03-21 13:46:58 +0100

    Se ha cambiado la forma en la que se generan los bloques. Ahora el generador es hijo del jugador. Los bloques mirarán al jugador una vez que son creados y se moveran siguiendo su eje forward propio.

--- VRProject/Assets/IsaacEscudero/Scripts/InstantiateCubes.cs
+++ VRProject/Assets/IsaacEscudero/Scripts/InstantiateCubes.cs
@@ -7,7 +7,7 @@ public class InstantiateCubes : MonoBehaviour
     [SerializeField] GameObject cubes;
     bool isInstantiate = false;
     float destinationOffsetRange;
-    [SerializeField]Transform spawnPos;
+    [SerializeField] Transform spawnPos;
     public static InstantiateCubes instance;
 
     private void Awake()
@@ -16,38 +16,33 @@ public class InstantiateCubes : MonoBehaviour
         {
             instance = this;
         }
-        else 
+        else
         {
             Destroy(this);
         }
-    }
-    public Transform GetPlayerPosition()
-    {
-        return player;
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         destinationOffsetRange = 5;
+        StartCoroutine(InstantianteDelay());
     }
-
-    // Update is called once per frame
-    void Update()
+    public Transform GetPlayerPosition()
     {
-        float offset = Random.Range(-destinationOffsetRange, destinationOffsetRange);
-        Vector3 direction = (new Vector3(Camera.main.transform.position.x + offset, Camera.main.transform.position.y + offset, Camera.main.transform.position.z + offset) - spawnPos.position).normalized;
-        if (!isInstantiate)
-        {
-            Vector3 cubePosition = 
-                new Vector3(spawnPos.position.x * direction.x, spawnPos.position.y * direction.y, spawnPos.position.z * direction.z);
-            GameObject c = Instantiate(cubes, cubePosition, Quaternion.identity);
-            StartCoroutine(InstantianteDelay());
-        }
+        return player;
     }
     IEnumerator InstantianteDelay()
     {
-        isInstantiate = true;
-        yield return new WaitForSeconds(2);
-        isInstantiate = false;
+        while (true)
+        {
+            float offset = Random.Range(-destinationOffsetRange, destinationOffsetRange);
+            Vector3 direction = (new Vector3(Camera.main.transform.position.x + offset, Camera.main.transform.position.y + offset, Camera.main.transform.position.z + offset) - spawnPos.position).normalized;
+            Vector3 cubePosition =
+                new Vector3(spawnPos.position.x + direction.x, direction.y + 1, spawnPos.position.z);
+            GameObject c = Instantiate(cubes, cubePosition, Quaternion.identity);
+            c.transform.LookAt(player);
+            yield return new WaitForSeconds(2);
+
+        }
     }
 }
